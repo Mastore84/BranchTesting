a0b72a67ce96627dcf454c760a79a65d41c529f3
Mastore84 <mpb.petersen@gmail.com>
2022-11-14 10:43:41 +0100

    added showMood feature

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -3,9 +3,18 @@ public class Dog extends Animal{
     public void makeSound(){
         System.out.println("Woof");
     }
-
     @Override
     public boolean eat(String foodType) {
         return true;
+    }
+
+    @Override
+    public void showMood(boolean mood) {
+        if(mood == true) {
+            System.out.println("logrer");
+        }
+        else if (mood == false) {
+            System.out.println("knurrer");
+        }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,5 +9,10 @@ public class Main {
 
         cat.eat("fish");
         dog.eat("dogfood");
+
+        dog.showMood(true);
+        dog.showMood(false);
+        cat.showMood(true);
+        cat.showMood(false);
     }
 }
